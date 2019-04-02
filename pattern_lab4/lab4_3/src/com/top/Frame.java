e622e66ae89b1e269203d50274504998a74db590
choigone <gowoonvv@gmail.com>
2019-04-02 22:18:56 +0900

    modified frame

--- pattern_lab4/lab4_3/src/com/top/Frame.java
+++ pattern_lab4/lab4_3/src/com/top/Frame.java
@@ -1,9 +1,7 @@
 package com.top;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.util.Vector;
 
 public class Frame extends JFrame {
 
@@ -11,17 +9,13 @@ public class Frame extends JFrame {
     JScrollPane listJs;
     JPanel listPanel;
 
-    DefaultTableModel model;
-    Vector<String> userRow;
-
     Rocket[] rockets = new Rocket[]{new Rocket("Shooter",3.95, new Length(50.0)), new Rocket("Orbit",29.95, new Length(5000.0))};
-    RocketTable rt = new RocketTable(rockets);
+    RocketTable rocketTable = new RocketTable(rockets);
 
     public Frame(String title){
         super(title);
 
-        model = new DefaultTableModel(rt.columnNames,0);
-        userTable = new JTable(model);
+        userTable = new JTable(rocketTable);
 
         setBounds(150,100,200,200);
         setResizable(false);
@@ -36,12 +30,5 @@ public class Frame extends JFrame {
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 
-        for(int i=0;i<rt.getRowCount();i++){
-            userRow = new Vector<String>();
-            for(int j=0;j<rt.getColumnCount();j++){
-                userRow.addElement(rt.getValueAt(i,j).toString());
-            }
-            model.addRow(userRow);
-        }
     }
 }
